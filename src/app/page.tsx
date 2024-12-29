3ff32971407a691ce224bde353246b53897fc688
samia <151353023+ssamiaa@users.noreply.github.com>
2024-12-28 19:49:46 -0800

    added a contact me page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
+import Contact from "@/components/Contact";
 
 export default function Home() {
     return (
@@ -13,6 +14,7 @@ export default function Home() {
             <About />
             <Projects/>
             <Experience/>
+            <Contact/>
             <Footer />
         </>
       );
